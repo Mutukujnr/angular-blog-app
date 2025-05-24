d08f3a3d9dcb7b7b82cb6e4b8cc9114f9d608ca1
amos <mutukujuniour@gmail.com>
2025-05-24 13:45:30 +0300

    prepared and designed the front view 1. set global styling

---
+++ src/app/layouts/component-navbar/component-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-navbar',
+  imports: [],
+  templateUrl: './component-navbar.component.html',
+  styleUrl: './component-navbar.component.css'
+})
+export class ComponentNavbarComponent {
+
+}
---
+++ src/app/subcription-form/subcription-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subcription-form',
+  imports: [],
+  templateUrl: './subcription-form.component.html',
+  styleUrl: './subcription-form.component.css'
+})
+export class SubcriptionFormComponent {
+
+}
